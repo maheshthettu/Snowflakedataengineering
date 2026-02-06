58a8391d76d2ab66b0bd463c1977dedc0460173e
maheshthettu <mahesh62726@gmail.com>
2026-02-06 12:08:33 +0530

    updated the code for token authentication

--- db_connection/config.py
+++ db_connection/config.py
@@ -1,8 +1,25 @@
+# #Password Authentication 
+
+# SNOWFLAKE_CONFIG = {
+#     "user": "MAHESH",
+#     "password": "Mahee@8374138092",
+#     "account": "SROTSLD-VP20091",
+#     "warehouse": "COMPUTE_WH",
+#     "database": "COLLEGE",
+#     "schema": "BRANCH"
+# }
+
+#token authenticaton 
+
+from dotenv import load_dotenv
+import os
+load_dotenv()  
+
 SNOWFLAKE_CONFIG = {
-    "user": "MAHESH",
-    "password": "Mahee@8374138092",
-    "account": "SROTSLD-VP20091",
-    "warehouse": "COMPUTE_WH",
-    "database": "COLLEGE",
-    "schema": "BRANCH"
+    "user": os.getenv("SNOWFLAKE_USER"),
+    "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+    "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+    "database": os.getenv("SNOWFLAKE_DATABASE"),
+    "schema": os.getenv("SNOWFLAKE_SCHEMA"),
+    "private_key_path": os.getenv("SNOWFLAKE_PRIVATE_KEY_PATH")
 }
--- db_connection/db_connecter.py
+++ db_connection/db_connecter.py
@@ -1,11 +1,40 @@
+
+# #password authentication 
+
+# import snowflake.connector
+# from db_connection.config import SNOWFLAKE_CONFIG
+
+# def get_connection():
+#     return snowflake.connector.connect(
+#         user=SNOWFLAKE_CONFIG["user"],
+#         password=SNOWFLAKE_CONFIG["password"],
+#         account=SNOWFLAKE_CONFIG["account"],
+#         warehouse=SNOWFLAKE_CONFIG["warehouse"],
+#         database=SNOWFLAKE_CONFIG["database"],
+#         schema=SNOWFLAKE_CONFIG["schema"]
+#     )
+
+
+
+#token authentication 
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 import snowflake.connector
 from db_connection.config import SNOWFLAKE_CONFIG
 
 def get_connection():
+    with open(SNOWFLAKE_CONFIG["private_key_path"], "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+
     return snowflake.connector.connect(
         user=SNOWFLAKE_CONFIG["user"],
-        password=SNOWFLAKE_CONFIG["password"],
         account=SNOWFLAKE_CONFIG["account"],
+        private_key=private_key,
         warehouse=SNOWFLAKE_CONFIG["warehouse"],
         database=SNOWFLAKE_CONFIG["database"],
         schema=SNOWFLAKE_CONFIG["schema"]
